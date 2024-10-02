639c8ab04e299234578896a1355b29e2cbc72be9
jai-dutta <161834694+jai-dutta@users.noreply.github.com>
2024-10-02 18:58:49 +0800

    add/test main harness

--- src/main.c
+++ src/main.c
@@ -1,11 +1,11 @@
 #include "game.h"
+#include "map.h"
 #include <stdio.h> /*TEMP*/
 #include <string.h>
 
 int main(int argc, const char* argv[]) {
-
-    /* argument checks */
-
+    Map* map;
+    FILE* map_file;
     /* Argument Check */
     if(argc != 2) {
         printf("Usage: ./treasure <map_file.txt>");
@@ -16,6 +16,17 @@ int main(int argc, const char* argv[]) {
         printf("Usage: ./treasure <map_file.txt>");
         return 1;
     }
+    printf("Trying to open file: %s\n", argv[1]);
+    map_file = fopen(argv[1], "r");
+    if(map_file == NULL) {
+        perror("fopen");
+    }
 
+    map = create_map(map_file);
+    if(read_map(map_file, map) != 0) {
+        printf("Error reading map");
+        return 1;
+    }
+    print_map(map);
     return 0;
 }
